223d5410d5fda3e8bda187d81e9a0eee41887cf0
dukekunyura <dukekunyura@gmail.com>
2024-01-19 15:58:13 +0300

    [+] add validation

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,38 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import { createUserDto } from './dto/createUser.dto';
 
-@Controller()
+@Controller('app')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('hello')
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('user/id=:id/type=:type')
+  getUser(
+    @Param('id', ParseIntPipe) id: number,
+    @Param('type', ParseIntPipe) type: number,
+  ) {
+    console.log(id + type);
+    return id + type;
+  }
+
+  @UsePipes(new ValidationPipe())
+  @Post('user/add')
+  createUser(@Body() dto: createUserDto) {
+    console.log(dto.name + ' ' + dto.surname);
+    return dto.name + ' ' + dto.surname;
   }
 }

e98e44dfd104aeccf7bf20983dcb454a3b6536f1
dukekunyura <dukekunyura@gmail.com>
2024-01-24 00:50:50 +0300

    [+] add config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { TelegrafModule } from 'nestjs-telegraf';
 import { TelegramModule } from './telegram/telegram.module';
+import { ConfigModule } from './config.module';
 
 @Module({
   imports: [
+    ConfigModule,
     TelegrafModule.forRoot({
       token: process.env.TG_TOKEN,
       include: [TelegramModule],
